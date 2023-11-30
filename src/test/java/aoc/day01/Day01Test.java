1b6fb284efcc6210c9665b2a6a4984e85a695e9f
Dave Burke <dburke84@gmail.com>
2023-11-30 17:58:05 -0600

    Update to Gradle 8.5, Java 21, JUnit 5

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -1,11 +1,12 @@
 package aoc.day01;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test {
 
