795cbe65160486411f8fd74c88276c3b252d146e
Marc Beck König <mbkj@tutamail.com>
2024-12-05 14:08:54 +0100

    add Day00 template boilerplate

---
+++ src/main/java/aoc/day00/Day00.java
@@ -0,0 +1,16 @@
+package aoc.day00;
+
+import aoc.Day;
+
+public class Day00 implements Day {
+
+    @Override
+    public String part1(String input) {
+        return String.valueOf(1);
+    }
+
+    @Override
+    public String part2(String input) {
+        return String.valueOf(2);
+    }
+}
