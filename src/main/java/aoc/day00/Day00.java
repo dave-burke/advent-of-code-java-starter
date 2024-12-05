5cc3983466d85fec51922cfca818dafa57f0e83d
Marc Beck König <mbkj@tutamail.com>
2024-12-05 14:11:15 +0100

    make day00 conform to day01 approach

--- src/main/java/aoc/day00/Day00.java
+++ src/main/java/aoc/day00/Day00.java
@@ -1,16 +1,19 @@
 package aoc.day00;
 
 import aoc.Day;
+import aoc.Utils;
+import java.util.List;
 
 public class Day00 implements Day {
 
     @Override
     public String part1(String input) {
-        return String.valueOf(1);
+        List<String> lines = Utils.splitLines(input);
+        return lines.isEmpty() ? "" : lines.get(0);
     }
 
     @Override
     public String part2(String input) {
-        return String.valueOf(2);
+        return input;
     }
 }
